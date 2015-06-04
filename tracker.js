1114200b38f0dde010bec2281b73333314e0dc3a
Matthew Stagg <mmstagg1@eagles.usi.edu>
2015-06-04 13:55:03 -0500

    Revert "Assets move along paths"
    This reverts commit 9c1b2966477ce796272fe27198ebfb02f1a2bd63.

--- tracker.js
+++ tracker.js
@@ -11,7 +11,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;   
 var stage = new createjs.Stage("trackerWindow");
 var img = new createjs.Bitmap("testsvg.svg");
-img.image.onload = mapLoad;
+img.image.onload = load;
 createjs.Ticker.setFPS(60);
 
 // Intitalize events
@@ -20,60 +20,35 @@ stage.addEventListener("stagemousedown", onMouseDown);
 canvas.addEventListener("mousewheel", MouseWheelHandler, false);
 canvas.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
 
+// Test circle
+var c = new createjs.Shape();
+c.graphics.beginFill("red").drawCircle(0, 0, 10);
+stage.addChild(c);
+
+// 'Main' Execution loop, fires 60 times per second
+function tick(e){
+
+}
+
 // Resizes SVG file after image loads and displays it
-// Remainder of program waits ot execute until after blueprint is loaded!
-function mapLoad(e){
+function load(e){
 	img.scaleX = img.scaleY = Math.min(canvas.width / img.image.width, canvas.height / img.image.height);
 	stage.addChild(img);
-	main();
 }
 
-// Main function
-function main(){
-	//Test shapes
-	var shp1 = new Asset("Yellow", 0, 0);
-	var shp2 = new Asset("Red", 0, 0);
-	var shp3 = new Asset("Green", 0, 0);
-	var shp4 = new Asset("Blue", 0, 0);
-	var shp5 = new Asset("Purple", 0, 0);
-	alert(img.image.width);
-}
-
-// Asset Structure
-function Asset(c, v, d){
-	this.shape = new createjs.Shape();
-	this.color = c;
-	this.shape.graphics.beginFill(this.color).drawCircle(0, 0, 10);			//TODO: Give assets shape that can indicate direction
-	this.velocity = v;														//TODO: Implement velocity that will control tweening speed
-	this.direction = d;														//TODO: Implement directional facing
-	this.movingToPoint = false;
-	this.path = generatePoints();											//TODO: Refactor into dynamic structure. Current implementation is static and beyond disgusting.
-	this.current = this.path.head;
-	stage.addChild(this.shape);
-	createjs.Tween.get(this.shape, { override:true, loop:true })
-	.to({x:this.path.head.x, y:this.path.head.y}, 1000, createjs.Ease.Linear)
-	.to({x:this.path.head.next.x, y:this.path.head.next.y}, 1000, createjs.Ease.Linear)
-	.to({x:this.path.head.next.next.x, y:this.path.head.next.next.y}, 1000, createjs.Ease.Linear)
-	.to({x:this.path.head.next.next.next.x, y:this.path.head.next.next.next.y}, 1000, createjs.Ease.Linear)
-	.to({x:this.path.head.next.next.next.next.x, y:this.path.head.next.next.next.next.y}, 1000, createjs.Ease.Linear)
-	.to({x:this.path.head.x, y:this.path.head.y}, 1000, createjs.Ease.Linear);
-}
-
-// Generates Linked List that represents path for asset to travel along
-function generatePoints(){
-	var a = {x:0, y:0};
-	var b = {x:(Math.floor((Math.random() * (img.image.width * img.scaleX)) - 1)), y:(Math.floor((Math.random() * (img.image.height * img.scaleY)) - 1))};
-	var c = {x:(Math.floor((Math.random() * (img.image.width * img.scaleX)) - 1)), y:(Math.floor((Math.random() * (img.image.height * img.scaleY)) - 1))};
-	var d = {x:(Math.floor((Math.random() * (img.image.width * img.scaleX)) - 1)), y:(Math.floor((Math.random() * (img.image.height * img.scaleY)) - 1))};
-	var e = {x:(Math.floor((Math.random() * (img.image.width * img.scaleX)) - 1)), y:(Math.floor((Math.random() * (img.image.height * img.scaleY)) - 1))};
-	
-	a.next = b;
-	b.next = c;
-	c.next = d;
-	d.next = e;
-	
-	return {head:a};
-}
+// Test function to handle dot movement, to be replaced with predefined movement apths for assets
+window.onkeydown = function (e) {
+    var code = e.keyCode ? e.keyCode : e.which;
+    if (code === 38) { //up key
+		createjs.Tween.get(c, { override:true }).to({y: c.y - 10}, 250, createjs.Ease.Linear);
+    } else if (code === 40) { //down key
+        createjs.Tween.get(c, { override:true }).to({y: c.y + 10}, 250, createjs.Ease.Linear);
+    } else if (code === 39) { //right key
+		createjs.Tween.get(c, { override:true }).to({x: c.x + 10}, 250, createjs.Ease.Linear);
+	} else if (code === 37) { //left key
+		createjs.Tween.get(c, { override:true }).to({x: c.x - 10}, 250, createjs.Ease.Linear);
+	}
+};
 
 // Listen for mouse click Events, handles dragging
 function onMouseDown(e) {
